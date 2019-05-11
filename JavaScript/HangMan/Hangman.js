419e26ff325a3e3f07f168a0402ab8b3df5427ac
seggiepants <41271733+seggiepants@users.noreply.github.com>
2019-05-11 12:08:38 -0600

    Finally got something to show. Not hangaman yet.

--- JavaScript/HangMan/Hangman.js
+++ JavaScript/HangMan/Hangman.js
@@ -1,3 +1,20 @@
 /* 
 Hangman.js
 */
+
+let cxt;
+
+function setup() {
+    let canvasEl;
+    canvasEl = document.getElementById("game");
+    ctx = canvasEl.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 639, 479);
+
+    ctx.strokeStyle = "white";
+    ctx.moveTo(0, 0);
+    ctx.lineTo(639, 479);
+    ctx.moveTo(639, 0);
+    ctx.lineTo(0, 479);
+    ctx.stroke();
+}
